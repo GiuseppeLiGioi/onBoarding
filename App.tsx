8fdd69f5a4b35b173d5704f6443861e82aceabb4
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-11-14 11:12:06 +0100

    added. expo StatusBar to change the color of the header and the icons it contains. I'm starting to implement new expo features and improve the interface.

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { AppProvider } from './src/contexts/AppContext';
 import HomeScreen from './src/screens/HomeScreen';
 import New1Screen from './src/screens/New1Screen';
 import New2Screen from './src/screens/New2Screen';
+import { SafeAreaView } from 'react-native-safe-area-context'; /*Si usa per evitare che gli elementi vengano disposti in punti non visbili sul dispositivo, dal nome li posiziona nelle parti visibili delo schermo.  */
 
 export default function App() {
 
@@ -12,6 +14,11 @@ export default function App() {
   return (
     /*Wrap degli screens dentro il Provider in modo che possano accedere ai valori definiti nel context */
     <AppProvider>
+
+     {/*Attualmente sfondo nero + icone bianche */}
+    <SafeAreaView style={{padding: 3, backgroundColor: "#0e0d0dff"}}/> 
+    {/*Mi consente di modificare il colore delle icone nella parte alta del dispositivo (impo, batteria, wi-fi, orario etc etc) */}
+    <StatusBar style="light"/> 
 
     <NavigationContainer>
       
