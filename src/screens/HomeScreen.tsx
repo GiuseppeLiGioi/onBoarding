12d56bd2c2c017272a78c8f8f942728e302f0aa3
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-11-14 16:32:15 +0100

    added. prettier code formatter

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,68 +1,69 @@
-import { View, TextInput, Text, TouchableOpacity } from "react-native"
-import { styles } from "../styles/common"
-import { useState } from "react"
-import { useAppContext } from "../contexts/AppContext"
-
+import { View, TextInput, Text, TouchableOpacity } from "react-native";
+import { styles } from "../styles/common";
+import { useState } from "react";
+import { useAppContext } from "../contexts/AppContext";
 
 export default function HomeScreen() {
-    const { stateToView, updateState, addTask, tasks, deleteTask } = useAppContext()
-    const [title, setTitle] = useState<string>("")
+  const { stateToView, updateState, addTask, tasks, deleteTask } =
+    useAppContext();
+  const [title, setTitle] = useState<string>("");
 
-    function handleSubmit() {
-        if (title) {
-            addTask(title);
-            setTitle('')
-        }
+  function handleSubmit() {
+    if (title) {
+      addTask(title);
+      setTitle("");
     }
+  }
 
-     function handleDelete(id: number) {
-        if (id) {
-            deleteTask(id);
-        }
+  function handleDelete(id: number) {
+    if (id) {
+      deleteTask(id);
     }
+  }
 
-
-    return (
-        <View style={styles.container}>
-            <View style={styles.containerInput}>
-
-                <TextInput
-                    placeholder="Inserisci il testo del task"
-                    placeholderTextColor="black"
-                    value={title}
-                    onChangeText={setTitle} /* Medesimo di scrivere: updateState((text) => setStateToView(text)) in Native ricevo già la stringa pronta
+  return (
+    <View style={styles.container}>
+      <View style={styles.containerInput}>
+        <TextInput
+          placeholder="Inserisci il testo del task"
+          placeholderTextColor="black"
+          value={title}
+          onChangeText={
+            setTitle
+          } /* Medesimo di scrivere: updateState((text) => setStateToView(text)) in Native ricevo già la stringa pronta
                                         Non  devo estrarre dall'evento (e) come in React Web.
                                         Con onChange normale updateState((e) => setStateToView(e.target.value))*/
-                    style={styles.inputStyle}
-                />
+          style={styles.inputStyle}
+        />
 
-                <TouchableOpacity style={styles.buttonAdd} onPress={() => handleSubmit()}>
-                    <Text style={styles.textAdd}>Aggiungi Task</Text>
-                </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.buttonAdd}
+          onPress={() => handleSubmit()}
+        >
+          <Text style={styles.textAdd}>Aggiungi Task</Text>
+        </TouchableOpacity>
+      </View>
+
+      {tasks && tasks.length > 0 && (
+        <View style={styles.taskList}>
+          {tasks.map((t) => (
+            <View style={styles.containerSingleTask}>
+              <Text
+                key={t.id}
+                style={{ marginBottom: 5, color: "white", textAlign: "center" }}
+              >
+                {t.title}
+              </Text>
+              <TouchableOpacity
+                style={styles.deleteButton}
+                onPress={() => handleDelete(t.id)}
+              >
+                <Text style={styles.textDelete}>Rimuovi Task</Text>
+              </TouchableOpacity>
             </View>
-
-            {
-                tasks && tasks.length > 0 && (
-                    <View style={styles.taskList}>
-                        {
-                            tasks.map((t) => (
-                                <View style={styles.containerSingleTask}>
-                                    <Text key={t.id} style={{ marginBottom: 5, color: "white", textAlign: "center" }}>{t.title}</Text>
-                                     <TouchableOpacity style={styles.deleteButton} onPress={() => handleDelete(t.id)}>
-                                            <Text style={styles.textDelete}>Rimuovi Task</Text>
-                                        </TouchableOpacity>
-                                </View>
-
-
-
-                            ))
-                        }
-                    </View>
-                )
-            }
+          ))}
         </View>
-
-
-
-    )
+      )}
+    </View>
+  );
 }
